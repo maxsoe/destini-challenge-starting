31b102604a8d5744843388e9cff4211ed844c3b8
Max Soe <maxsoe@gmail.com>
2023-01-10 00:40:12 +0400

    117 Step 25 - Renaming and Refactoring Using Android Studio

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -34,35 +34,33 @@ class StoryBrain {
   ];
 
   String getStory() {
-    return _storyData[storyNumber].storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
   String getChoice1() {
-    return _storyData[storyNumber].choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
   String getChoice2() {
-    return _storyData[storyNumber].choice2;
+    return _storyData[_storyNumber].choice2;
   }
 
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-  int storyNumber = 0;
+  int _storyNumber = 0;
 
   void nextStory(int choiceNumber) {
-    if (choiceNumber == 1 && storyNumber == 0) {
-      storyNumber = 2;
-    } else if (choiceNumber == 2 && storyNumber == 0) {
-      storyNumber = 1;
-    } else if (choiceNumber == 1 && storyNumber == 2) {
-      storyNumber = 5;
-    } else if (choiceNumber == 2 && storyNumber == 2) {
-      storyNumber = 4;
-    } else if (choiceNumber == 1 && storyNumber == 1) {
-      storyNumber = 2;
-    } else if (choiceNumber == 2 && storyNumber == 1) {
-      storyNumber = 3;
-    } else if (storyNumber == 3 || storyNumber == 4 || storyNumber == 5) {
+    if (choiceNumber == 1 && _storyNumber == 0) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 0) {
+      _storyNumber = 1;
+    } else if (choiceNumber == 1 && _storyNumber == 2) {
+      _storyNumber = 5;
+    } else if (choiceNumber == 2 && _storyNumber == 2) {
+      _storyNumber = 4;
+    } else if (choiceNumber == 1 && _storyNumber == 1) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 1) {
+      _storyNumber = 3;
+    } else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
       restart();
     }
   }
