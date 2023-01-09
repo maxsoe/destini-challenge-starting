5ad218e1f4b888d43c9e5ed6ca6459f72242681b
Max Soe <maxsoe@gmail.com>
2023-01-09 23:35:31 +0400

    114 Update the Story Based on User Choice

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'story_brain.dart';
 import 'package:flutter/material.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-
 void main() => runApp(Destini());
 
 class Destini extends StatelessWidget {
@@ -49,7 +47,7 @@ class _StoryPageState extends State<StoryPage> {
                 child: TextButton(
                   onPressed: () {
                     //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    storyBrain.nextStory(1);
                   },
                   style: TextButton.styleFrom(
                       backgroundColor: Colors.red,
@@ -72,7 +70,7 @@ class _StoryPageState extends State<StoryPage> {
                 child: TextButton(
                   onPressed: () {
                     //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    storyBrain.nextStory(2);
                   },
                   style: TextButton.styleFrom(
                       backgroundColor: Colors.blue,
